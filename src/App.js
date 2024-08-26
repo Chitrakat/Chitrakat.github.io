54d68fc13400668d41cdb407f4367ebaa32dcd66
Suyash Chitrakar <schitrakar@knox.edu>
2024-08-26 08:01:49 -0500

    Added basic functinality to the website

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './App.css';
 
-// components will imported from  inside containers
-
-export const App = () => {
+function App() {
   return (
-    <div>
-        <h1>Hello-World</h1>
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Website</h1>
+      </header>
+      <main>
+        <p>Hello, World!</p>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
-// npm start
+export default App;
